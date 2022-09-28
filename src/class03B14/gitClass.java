facea0c599c98fac2e840cafc457f1178eb8f4de
ZYNZYK <zinaidacantir@gmail.com>
2022-09-27 20:07:00 -0400

    the third commit

--- src/class03B14/gitClass.java
+++ src/class03B14/gitClass.java
@@ -6,6 +6,7 @@ public class gitClass {
 		
 		System.out.println("Hey this is a git class");
 		System.out.println("Hey this is a change in my project");
+		System.out.println("how are u this a new version");
 	}
 
 }
